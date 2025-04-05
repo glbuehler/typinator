4b82f4b7ccc9e7d6928eccdc22aba7f92d84c5c4
Gideon Bühler <gideonbuehler18@gmail.com>
2025-04-05 20:08:05 +0200

    prettier result screen

--- src/render/menu.rs
+++ src/render/menu.rs
@@ -1,26 +1,57 @@
 use std::io::{self, Write};
 
+use crossterm::terminal;
+
 use super::*;
 use crate::race;
 
 pub fn render_menu(race_result: race::RaceInfo) {
+    let time = format!("{:.2}s", race_result.duration.as_secs_f32());
+    let wpm = if !race_result.duration.is_zero() {
+        format!(
+            "{:.2}",
+            (race_result.words as f32 / (race_result.duration.as_secs_f32() / 60.0))
+        )
+    } else {
+        "N/A".to_string()
+    };
+    let accuracy = if race_result.characters != 0 {
+        format!(
+            "{:.2}%",
+            (100.0 * (1.0 - (race_result.mistakes as f32 / race_result.characters as f32)))
+        )
+    } else {
+        "N/A".to_string()
+    };
+
+    render_results(&[
+        ("Words typed", &race_result.words.to_string()),
+        ("Letters typed", &race_result.characters.to_string()),
+        ("Mistakes", &race_result.mistakes.to_string()),
+        ("Time", &time),
+        ("WPM", &wpm),
+        ("Accuracy", &accuracy),
+    ])
+}
+
+fn render_results(res: &[(&str, &str)]) {
+    let (w, h) = terminal::size().unwrap();
+    let (w, h) = (w as usize, h as usize);
+    let mut row = (h - res.len()) / 2;
+
     let mut buf = vec![];
     buf.extend(CLEAR_SCREEN);
-    buf.extend(RESET_CURSOR);
     buf.extend(HIDE_CURSOR);
 
-    buf.extend(
-        format!(
-            "Words typed:\t{}\r\nLetters typed:\t{}\r\nMistakes:\t{}\r\nTime:\t\t{:.2}s\r\nWPM:\t\t{:.2}\r\nAccuracy:\t{:.2}%",
-            race_result.words,
-            race_result.characters,
-            race_result.mistakes,
-            race_result.duration.as_secs_f32(),
-            race_result.words as f32 / (race_result.duration.as_secs_f32() / 60.0),
-            100.0 * (1.0 - (race_result.mistakes as f32 / race_result.characters as f32)),
-        )
-        .as_bytes(),
-    );
+    for (l, r) in res {
+        let col = w / 2 - l.chars().count() - 1;
+        buf.extend(move_cursor_to(col, row));
+        buf.extend(l.as_bytes());
+        buf.push(b' ');
+        buf.extend(r.as_bytes());
+
+        row += 1;
+    }
 
     io::stdout().write_all(&buf).unwrap();
     io::stdout().flush().unwrap();
